fa898c116de150b64fe76c59c1781d2d98ade044
panzhiyong <sd4015700@126.com>
2021-06-26 21:27:34 +0800

    feat: 初始提交

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // 一行最多 200 字符
+    printWidth: 200,
+    // 使用单引号
+    singleQuote: true,
+    // 使用 4 个空格缩进
+    tabWidth: 4,
+    // 不使用缩进符，而使用空格
+    useTabs: false,
+    // 行尾需要有分号
+    semi: false,
+    // 避免报: delete (cr)的错
+    endOfLine: "auto",
+    // 在jsx中把'>' 单独放一行
+    jsxBracketSameLine: false,
+    trailingComma:'none'
+}
