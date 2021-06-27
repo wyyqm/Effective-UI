1877115008cfab06caa038a8ad7a41d783809724
pan-zy <sd4015700@yeah.net>
2021-06-27 10:55:05 +0800

    测试git钩子是否有效

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,15 +3,15 @@ module.exports = {
     printWidth: 200,
     // 使用单引号
     singleQuote: true,
-    // 使用 4 个空格缩进
-    tabWidth: 4,
+    // 使用 2 个空格缩进
+    tabWidth: 2,
     // 不使用缩进符，而使用空格
     useTabs: false,
     // 行尾需要有分号
     semi: false,
     // 避免报: delete (cr)的错
-    endOfLine: "auto",
+    endOfLine: 'auto',
     // 在jsx中把'>' 单独放一行
     jsxBracketSameLine: false,
-    trailingComma:'none'
+    trailingComma: 'none'
 }
