af18be947f71414e3b236ec495cfbffcc9442bfe
tianbian <tianbian.wang@tuya.com>
2021-12-02 11:36:03 +0800

    feat 组件封装

--- examples/router/index.js
+++ examples/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Input from '../components/input/index'
+import Search from '../components/search/index'
 import searchRow from '../pages/searchRow/index'
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/input',
     name: 'input',
     component: Input
+  },
+  {
+    path: '/input',
+    name: 'ss',
+    component: Search
   },
   {
     path: '/search',
